f5e853418b786da0b6fea0c35253d142e7c28600
zeke_yang <18174019209@163.com>
2020-12-11 22:39:46 +0800

    重写后台

--- src/config.js
+++
@@ -1,6 +0,0 @@
-import {join} from 'path'
-export default {
-    publicPath: join(__dirname, './../public'),
-    viewsPath: join(__dirname, './../views'),
-    uploadPath: join(__dirname, './../public/uploads')
-}
